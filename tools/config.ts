f1be2ee727a2cb33a8222cb3807e7bae3e7823b0
doh <dag.heradstveit@nsd.uib.no>
2015-11-20 14:59:08 +0100

    Updated toolchain to seed master

--- tools/config.ts
+++ tools/config.ts
@@ -1,95 +1,77 @@
 import {readFileSync} from 'fs';
 import {argv} from 'yargs';
 
-let resolve = require.resolve;
 
 // --------------
 // Configuration.
-export const ENV              = argv['env']         || 'dev';
-export const DEBUG            = argv['debug']       || false;
+export const ENV                  = argv['env']         || 'dev';
+export const DEBUG                = argv['debug']       || false;
+export const PORT                 = argv['port']        || 5555;
+export const LIVE_RELOAD_PORT     = argv['reload-port'] || 4002;
+export const DOCS_PORT            = argv['docs-port']   || 4003;
+export const APP_BASE             = argv['base']        || '/';
 
-export const PORT             = argv['port']        || 5555;
-export const LIVE_RELOAD_PORT = argv['reload-port'] || 4002;
-export const DOCS_PORT        = argv['docs-port']   || 4003;
-export const APP_BASE         = argv['base']        || '/';
+export const APP_TITLE            = 'qddt-client';
 
-export const APP_SRC          = 'app';
-export const APP_DEST         = 'dist';
-export const APP_DOCS         = 'docs';
-export const ANGULAR_BUNDLES  = './node_modules/angular2/bundles';
-export const VERSION          = version();
+export const APP_SRC              = 'app';
+export const ASSETS_SRC           = `${APP_SRC}/assets/**/*`;
 
-export const VERSION_NPM      = '3.0.0';
-export const VERSION_NODE     = '4.0.0';
+export const TOOLS_DIR            = 'tools';
+export const TEST_DEST            = 'test';
+export const DOCS_DEST            = 'docs';
+export const APP_DEST             = `dist/${ENV}`;
+export const ASSETS_DEST          = `${APP_DEST}/assets`;
+export const CSS_DEST             = `${APP_DEST}/css`;
+export const FONTS_DEST           = `${APP_DEST}/fonts`;
+export const LIB_DEST             = `${APP_DEST}/lib`;
+export const VERSION              = appVersion();
 
-export const PATH = {
-  cwd: process.cwd(),
-  tools: 'tools',
-  docs :`${APP_DEST}/${APP_DOCS}`,
-  dest: {
-    all: APP_DEST,
-    dev: {
-      all: `${APP_DEST}/${ENV}`,
-      lib: `${APP_DEST}/${ENV}/lib`,
-      css: `${APP_DEST}/${ENV}/css`,
-      assets: `${APP_DEST}/${ENV}/assets`,
-      fonts: `${APP_DEST}/${ENV}/fonts`
-    },
-    test: 'test',
-    tmp: '.tmp'
-  },
-  src: {
-    all: APP_SRC,
-    jslib_inject: [
-      // Order is quite important here for the HTML tag injection.
-      resolve('es6-shim/es6-shim.min.js'),
-      resolve('es6-shim/es6-shim.map'),
-      resolve('reflect-metadata/Reflect.js'),
-      resolve('reflect-metadata/Reflect.js.map'),
-      resolve('systemjs/dist/system.src.js'),
-      resolve('jquery/dist/jquery.js'),
-      resolve('materialize-css/dist/js/materialize.js'),
-      `${APP_SRC}/system.config.js`,
-      `${ANGULAR_BUNDLES}/angular2.dev.js`,
-      `${ANGULAR_BUNDLES}/router.dev.js`,
-      `${ANGULAR_BUNDLES}/http.dev.js`
-    ],
-    jslib_copy_only: [
-      resolve('systemjs/dist/system-polyfills.js'),
-      resolve('systemjs/dist/system-polyfills.js.map')
-    ],
-    csslib: [
-      resolve('materialize-css/dist/css/materialize.css')
-    ],
-    assets: [
-      `${APP_SRC}/assets/**/*`
-    ],
-    fonts: [
-      resolve('materialize-css/dist/font/roboto/Roboto-Bold.woff'),
-      resolve('materialize-css/dist/font/roboto/Roboto-Bold.woff2'),
-      resolve('materialize-css/dist/font/roboto/Roboto-Bold.ttf'),
-      resolve('materialize-css/dist/font/roboto/Roboto-Medium.woff'),
-      resolve('materialize-css/dist/font/roboto/Roboto-Medium.woff2'),
-      resolve('materialize-css/dist/font/roboto/Roboto-Medium.ttf'),
-      resolve('materialize-css/dist/font/roboto/Roboto-Regular.woff'),
-      resolve('materialize-css/dist/font/roboto/Roboto-Regular.woff2'),
-      resolve('materialize-css/dist/font/roboto/Roboto-Regular.ttf'),
-      resolve('materialize-css/dist/font/roboto/Roboto-Light.woff'),
-      resolve('materialize-css/dist/font/roboto/Roboto-Light.woff2'),
-      resolve('materialize-css/dist/font/roboto/Roboto-Light.ttf'),
-      resolve('materialize-css/dist/font/material-design-icons/Material-Design-Icons.eot'),
-      resolve('materialize-css/dist/font/material-design-icons/Material-Design-Icons.svg'),
-      resolve('materialize-css/dist/font/material-design-icons/Material-Design-Icons.ttf'),
-      resolve('materialize-css/dist/font/material-design-icons/Material-Design-Icons.woff'),
-      resolve('materialize-css/dist/font/material-design-icons/Material-Design-Icons.woff2')
-    ]
+export const VERSION_NPM          = '3.0.0';
+export const VERSION_NODE         = '4.0.0';
+
+
+export const DEV_DEPENDENCIES = [
+  { src: 'systemjs/dist/system-polyfills.js', dest: LIB_DEST },
+  { src: 'es6-shim/es6-shim.min.js', dest: LIB_DEST, inject: 'shims' },
+  { src: 'reflect-metadata/Reflect.js', dest: LIB_DEST, inject: 'shims' },
+  { src: 'systemjs/dist/system.src.js', dest: LIB_DEST, inject: 'shims' },
+  { src: 'angular2/bundles/angular2.dev.js', dest: LIB_DEST, inject: true },
+  { src: 'angular2/bundles/router.dev.js', dest: LIB_DEST, inject: true },
+  { src: 'angular2/bundles/http.dev.js', dest: LIB_DEST, inject: true },
+
+  { src: 'materialize-css/dist/js/materialize.js', dest: LIB_DEST, inject: true },
+  { src: 'materialize-css/dist/css/materialize.css', dest: CSS_DEST, inject: true },
+
+  { src: 'materialize-css/dist/font/roboto/Roboto-Bold.woff2',                          dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/roboto/Roboto-Bold.ttf',                            dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/roboto/Roboto-Medium.woff',                         dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/roboto/Roboto-Medium.woff2',                        dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/roboto/Roboto-Medium.ttf',                          dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/roboto/Roboto-Regular.woff',                        dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/roboto/Roboto-Regular.woff2',                       dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/roboto/Roboto-Regular.ttf',                         dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/roboto/Roboto-Light.woff',                          dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/roboto/Roboto-Light.woff2',                         dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/roboto/Roboto-Light.ttf',                           dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/material-design-icons/Material-Design-Icons.eot',   dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/material-design-icons/Material-Design-Icons.svg',   dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/material-design-icons/Material-Design-Icons.ttf',   dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/material-design-icons/Material-Design-Icons.woff',  dest: FONTS_DEST},
+  { src: 'materialize-css/dist/font/material-design-icons/Material-Design-Icons.woff2', dest: FONTS_DEST}
+
+];
+
+export const SYSTEM_CONFIG = {
+  defaultJSExtensions: true,
+  paths: {
+    '@reactivex/rxjs/dist/cjs/*': 'lib/@reactivex/*',
+    'angular2/*': 'lib/angular2/*'
   }
 };
 
-
 // --------------
 // Private.
-function version(): number|string {
+function appVersion(): number|string {
   var pkg = JSON.parse(readFileSync('package.json').toString());
   return pkg.version;
 }
