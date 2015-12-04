338c412424f8882741894f617c574b9aaa7f2cd5
Dag Østgulen Heradstveit <dagherad@gmail.com>
2015-12-04 16:50:03 +0100

    Fixed date format and extended to two formats.

--- app/common/date_pipe.ts
+++ app/common/date_pipe.ts
@@ -1,21 +1,28 @@
 import {Component, Pipe} from 'angular2/angular2';
 
 @Pipe({
+  name: 'localDate',
+  pure: true
+})
+export class LocalDatePipe {
+
+  transform(input: Array<number>): string {
+    var date: Date = new Date();
+    date.setUTCFullYear(input[0], input[1]-1, input[2]);
+    return date.toDateString();
+  }
+}
+
+@Pipe({
   name: 'localDateTime',
   pure: true
 })
 export class LocalDateTimePipe {
 
   transform(input: Array<number>): string {
-
     var date: Date = new Date();
-    date.setFullYear(input[0]);
-    date.setMonth(input[1]);
-    date.setDate(input[2]);
-    date.setHours(input[3]);
-    date.setMinutes(input[4]);
-    date.setSeconds(input[5]);
-    date.setMilliseconds(input[6]);
+    date.setUTCFullYear(input[0], input[1]-1, input[2]);
+    date.setUTCHours(input[3], input[4], input[5]);
 
     return date.toString();
   }
