ff56003345a6a5687b1d543b767e4410dbefe596
Dag Østgulen Heradstveit <dagherad@gmail.com>
2015-12-14 08:22:21 +0100

    Fixed error in sass scripts

--- tools/tasks/build.sass.dev.ts
+++ tools/tasks/build.sass.dev.ts
@@ -4,7 +4,7 @@ import {APP_SRC} from '../config';
 export = function buildSassDev(gulp, plugins, option) {
   return function () {
     return gulp.src(join(APP_SRC, '**', '*.scss'))
-      .pipe(plugins.sass().on('error', LoginComponent.logError))
+      .pipe(plugins.sass().on('error', plugins.sass.logError))
       .pipe(gulp.dest(APP_SRC));
   };
 }
