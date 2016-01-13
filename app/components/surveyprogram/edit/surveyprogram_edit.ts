c78823b7f1841e257f2d32ad03933222712068d5
SNO <stig.norland@nsd.uib.no>
2016-01-13 15:31:21 +0100

    lagt til meta info i Survey

--- app/components/surveyprogram/edit/surveyprogram_edit.ts
+++ app/components/surveyprogram/edit/surveyprogram_edit.ts
@@ -3,44 +3,48 @@ import {Component, Input} from 'angular2/core';
 import {SurveyService, SurveyProgram} from '../../surveyprogram/surveyservice';
 
 @Component({
-  selector: 'surveyprogram-edit',
-  providers: [SurveyService],
-  template: `
+    selector: 'surveyprogram-edit',
+    providers: [SurveyService],
+    template: `
   <div *ngIf="isVisible">
     <div *ngIf="surveyProgram" class="card" id="{{surveyProgram.id}}"  >
-        <form (ngSubmit)="save()" #hf="ngForm">
-          <div class="row">
-            <div class="input-field col s12">
-              <input type="text" [(ngModel)]="surveyProgram.name" required>
-              <label for="name" class="active teal-text">Name</label>
-            </div>
+      <form (ngSubmit)="save()" #hf="ngForm">
+        <div class="row">
+          <div class="input-field col s12">
+            <input type="text" [(ngModel)]="surveyProgram.name" required>
+            <label for="name" class="active teal-text">Name</label>
           </div>
-          <div class="row">
-            <div class="input-field col s12">
-              <textarea class="materialize-textarea"  [(ngModel)]="surveyProgram.description" required></textarea>
-              <label for="description" class="active teal-text">Description</label>
-            </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <textarea class="materialize-textarea"  [(ngModel)]="surveyProgram.description" required></textarea>
+            <label for="description" class="active teal-text">Description</label>
           </div>
-          <div class="row">
-            <div class="input-field col s4">
-              <input type="text" [(ngModel)]="surveyProgram.changeReason" >
+        </div>
+        <div class="row">
+		  <div class="input-field col s4">
               <label class="active teal-text">Version Reason</label>
-            </div>
-            <div class="input-field col s8">
-              <input type="text" [(ngModel)]="surveyProgram.changeComment" required>
-              <label for="changeComment" class="active teal-text">Save Comment</label>
-            </div>
+              <select  class="browser-default"  [(ngModel)]="surveyProgram.changeKind">
+                <option *ngFor="#changereason of changes" [value]="changereason">{{changereason}}</option>
+              </select>
           </div>
-          <div class="row">
-            <div class="input-field col s12">
-                <p><label class="active teal-text">Authors</label></p>
-                
-                <div id="author1" class="chip">Admin <i class="material-icons">close</i></div>
-                <div id="author2" class="chip">Stig Norland <i class="material-icons">close</i></div>
-            </div>
+          <div class="input-field col s8">
+            <input type="text" [(ngModel)]="surveyProgram.changeComment" required>
+            <label for="changeComment" class="active teal-text">Save Comment</label>
           </div>
-      <button type="submit" class="btn btn-default">Submit</button>
-    </form>
+        </div>
+        <div class="row">
+          <div class="input-field col s8">
+            <p><label class="active teal-text">Authors</label></p>
+            <div class="chip" *ngFor="#author of surveyProgram.authors" ><img src="{{author.picture}}">{{author.name}} <i class="material-icons">close</i></div>
+          </div>
+          <div class="input-field col s4">
+            <p><label class="active teal-text">Agency</label></p>
+            <div class="chip" >{{surveyProgram.createdBy.agency.name}}</div>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
     </div>
   </div>
 
@@ -48,15 +52,14 @@ import {SurveyService, SurveyProgram} from '../../surveyprogram/surveyservice';
 })
 export class SurveyProgramEditComponent {
 
-  @Input() surveyProgram: SurveyProgram;
-  private service: SurveyService;
-
+    @Input() surveyProgram: SurveyProgram;
+    private service: SurveyService;
   constructor(surveyService: SurveyService) {
-    this.service = surveyService;
-  }
-
-  save() {
-    this.service.save(this.surveyProgram);
-  }
+      this.service = surveyService;
+      this.changes = ['IN_DEVELOPMENT','TYPO','NEW_MAJOR'];
+    }
+    save() {
+        this.service.save(this.surveyProgram);
+    }
 
 }
