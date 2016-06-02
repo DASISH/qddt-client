216a21114a751aece45bc0e03337ddaa081e27a9
StigNorland <stig.norland@nsd.no>
2016-06-02 09:20:43 +0200

    BUG FIX, concept.name -> concept.id as toc ref

--- src/components/concept/concept_toc.component.ts
+++ src/components/concept/concept_toc.component.ts
@@ -10,7 +10,7 @@ import { Component, Input} from 'angular2/core';
   ],
   template: `
     <ul class="toc-children" *ngFor="#concept of concepts">
-      <li><a *ngIf="concept.name" href="#{{concept.name}}">{{concept.name}}</a>
+      <li><a *ngIf="concept.name" href="#{{concept.id}}">{{concept.name}}</a>
       <concept-toc [concepts]=concept.children></concept-toc>
       </li>
     </ul>
