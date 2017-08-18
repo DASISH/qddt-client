9a6742f12dd4a6dd85d9c732bfdc79b6d7d49711
Stig Norland <stig.norland@gmail.com>
2017-08-18 10:14:50 +0200

    https://github.com/DASISH/qddt-client/issues/523

--- src/client/app/shared/form/version/version.component.ts
+++ src/client/app/shared/form/version/version.component.ts
@@ -6,7 +6,9 @@ import { Component, Input } from '@angular/core';
   template:
   `{{element.version?.major}}.{{element.version?.minor}}
     <i *ngIf="element.version?.versionLabel=='In Development'" class="material-icons white yellow-text tiny"
-    title="Latest changes, not saved as a version">error</i>`,
+    title="Latest changes, not saved as a version">error</i>
+    <i *ngIf="element.version?.versionLabel=='Changes in hierarchy'" class="material-icons white orange-text tiny"
+    title="Hierarchy changes, not saved as a version">error</i>`,
   providers: []
 })
 export class VersionComponent  {
