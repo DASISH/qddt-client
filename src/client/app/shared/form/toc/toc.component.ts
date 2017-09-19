681143d9558d0982e19c8268ae996ede6e27de96
Stig Norland <stig.norland@gmail.com>
2017-09-19 10:18:31 +0200

    ok

---
+++ src/client/app/shared/form/toc/toc.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'qddt-toc',
+  moduleId: module.id,
+  template: `
+    <div class="col hide-on-small-only m1 l1">
+      <div style="top: 55;" class="toc-wrapper pinned">
+        <h5>{{tocName}}</h5>
+        <ul class="section table-of-contents">
+          <li><a href="home#topMenu">To the top </a> </li>
+          <li *ngFor="let element of elements"><a href="home#{{element.id}}">{{element.name}}</a></li>
+        </ul>
+      </div>
+    </div> 
+`,
+  providers: []
+})
+export class TocComponent  {
+  @Input() tocName:string;
+  @Input() elements: any[];
+}
