a27b87fd8dc5077f4f293fd4458cabe0a2886cee
Stig Norland <stig.norland@gmail.com>
2017-09-23 11:38:23 +0200

    https://github.com/DASISH/qddt-client/issues/517

--- src/client/app/common/preview/responsedomain/datetime.component.ts
+++ src/client/app/common/preview/responsedomain/datetime.component.ts
@@ -1,39 +1,65 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ResponseDomain } from '../../../components/responsedomain/responsedomain.service';
 
 @Component({
   selector: 'qddt-preview-rd-datetime',
   moduleId: module.id,
-  template: `<div class="row" *ngIf="responseDomain">
+  template: `<div class="row" *ngIf="responseDomain.managedRepresentation">
                <span>
-               <label>{{low}}
-               <input  type="date" class="datepicker"/></label>
+               <label>{{low}} - {{high}}</label>
+               <input  type="text" class="datepicker" materialize="pickadate" [materializeParams]=
+                         "[{format:dFormat,selectYears:80, max: [high, 12,31], min: [low, 1,1] }]">
+                 <!---->
                </span>
              </div>`,
   styles: [],
 })
 
-export class ResponsedomainDatetimeComponent implements OnInit {
+export class ResponsedomainDatetimeComponent implements OnInit,OnChanges {
   @Input() responseDomain: ResponseDomain;
 
-  low: number;
+  private low: number;
   private high: number;
+  private dFormat: any;
+  private dateOptions: any;
 
   ngOnInit() {
     this.low = 0;
     this.high = 1;
-    if (this.responseDomain !== null && this.responseDomain !== undefined) {
+    this.ngOnChanges(null);
+  }
+
+  ngOnChanges(changes: any): void {
+    if (changes)
+      console.log(changes.toString());
+    if (this.responseDomain) {
       let rep = this.responseDomain.managedRepresentation;
-      if (rep !== undefined) {
-        if (rep.inputLimit !== undefined
-          && rep.inputLimit.maximum !== undefined) {
+      if (rep) {
+        if (rep.inputLimit.maximum) {
           this.high = rep.inputLimit.maximum;
         }
-        if (rep.inputLimit !== undefined
-          && rep.inputLimit.minimum !== undefined) {
+        if (rep.inputLimit.minimum) {
           this.low = rep.inputLimit.minimum;
         }
+        if (rep.format) {
+          this.dFormat = rep.format;
+        } else {
+          this.dFormat = 'd mmmm, yyyy!';
+        }
       }
+      this.dateOptions = this.getDefaultPickaOption();
     }
+  }
+
+  private getDefaultPickaOption(): any {
+    return Object.assign({}, {
+      selectMonths: true,
+      selectYears: 80,
+      format: this.dFormat,
+      max: [this.high, 12,31],
+      min: [this.low, 1,1],
+      editable: true,
+      closeOnSelect: true
+    });
   }
 }
