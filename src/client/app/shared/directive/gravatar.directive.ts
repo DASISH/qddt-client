0b730462e9a7129e94a1ee97f0cecc6fa916d600
Stig Norland <stig.norland@gmail.com>
2018-02-14 13:21:05 +0100

    lint fixes

---
+++ src/client/app/shared/directive/gravatar.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+// import * as md5 from 'md5';
+// var md5 = require('md5');
+@Directive({
+  selector: '[gravatar]'
+})
+export class GravatarDirective implements OnChanges , OnInit {
+  @Input('email') email: string;
+  @Input('size') size: number = 16;
+  @Input('fallback') fallback: string = 'mm';
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {
+    this.setUrlGravatar(this.email);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['email']) {
+      this.setUrlGravatar(changes['email'].currentValue);
+    }
+  }
+
+  setUrlGravatar(email: string) {
+    this.elementRef.nativeElement.src = `//www.gravatar.com/avatar/&d=${this.fallback}`;
+    // this.elementRef.nativeElement.src = `//www.gravatar.com/avatar/${md5(email)}?s=${this.size}&d=${this.fallback}`;
+  }
+}
