a4e60941b7963748847beede59a77fa401b94681
Stig Norland <stig.norland@gmail.com>
2018-03-02 16:26:35 +0100

    refact

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MaterializeAction } from 'angular2-materialize';
+
+@Component({
+  selector: 'qddt-header',
+  moduleId: module.id,
+  template: `
+  <div class="row teal z-depth-1" style="padding-left: 2%; padding-top: 1%; padding-bottom: 1%;">
+  <i class="material-icons large right">{{headericon}}</i>
+  <h4>{{headername}</h4>
+  <a class="btn" (click)="onToggleNewForm()">
+    <i class="material-icons right" *ngIf="!isNewFormVisible">keyboard_arrow_down</i>
+    <i class="material-icons right" *ngIf="isNewFormVisible">keyboard_arrow_up</i> New
+  </a>`,
+  providers: []
+})
+export class HeaderComponent  {
+    @Input() headername: any;
+    @Input() headericon: any;
+
+    @Output() showform = new EventEmitter<boolean>();
+
+    public isNewFormVisible: boolean;
+
+    onToggleNewForm() {
+        this.showform.emit(this.isNewFormVisible);
+    }
+
+}
+
+
+
+
+
