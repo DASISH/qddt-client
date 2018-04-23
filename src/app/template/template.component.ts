b59375ecedb11e742489265dee2f74d9362ef720
Stig Norland <stig.norland@gmail.com>
2018-04-23 07:28:42 +0200

    tempate fix

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -2,10 +2,12 @@ import { OnChanges, Component, SimpleChanges, OnDestroy} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { QddtMessageService } from '../core/global/message.service';
 import { Factory } from '../shared/classes/factory';
-import { IEntityEditAudit } from '../shared/classes/interfaces';
+import { IEntityEditAudit, IPageSearch } from '../shared/classes/interfaces';
 import { ActionKind, ElementKind } from '../shared/classes/enums';
 import { HEADER_DETAILS } from '../shared/classes/constants';
 import { TemplateService } from './template.service';
+import { QddtPropertyStoreService } from '../core/global/property.service';
+import { ResponseDomain } from '../responsedomain/responsedomain.classes';
 
 declare var Materialize: any;
 
@@ -26,17 +28,19 @@ export class TemplateComponent implements OnChanges, OnDestroy {
   public showForm = false;
   private kind: ElementKind;
   private alive = true;
+  private path: string;
 
 
-  constructor( private route: ActivatedRoute,  private  messages: QddtMessageService, private service: TemplateService ) {
+  constructor( private route: ActivatedRoute,  private  messages: QddtMessageService, private service: TemplateService,
+    private properties: QddtPropertyStoreService ) {
     this.route.url
     .takeWhile(() => this.alive)
     .subscribe((event) => {
-      const path = this.route.firstChild.routeConfig.path;
-      if (HEADER_DETAILS.has(path)) {
-        this.kind = HEADER_DETAILS.get(path).kind;
-        this.icon = HEADER_DETAILS.get(path).icon;
-        this.headerName =  HEADER_DETAILS.get(path).headerName;
+      this.path = this.route.firstChild.routeConfig.path;
+      if (HEADER_DETAILS.has(this.path)) {
+        this.kind = HEADER_DETAILS.get(this.path).kind;
+        this.icon = HEADER_DETAILS.get(this.path).icon;
+        this.headerName =  HEADER_DETAILS.get(this.path).headerName;
       }
     });
   }
@@ -53,7 +57,12 @@ export class TemplateComponent implements OnChanges, OnDestroy {
         this.messages.sendAction(  { id: '', action: ActionKind.Update, object: null });
       }
       if (this.showForm ) {
+        const page: IPageSearch =  this.properties.get(this.path);
         this.newItem = Factory.createInstance(this.kind);
+        if (page.kind === ElementKind.RESPONSEDOMAIN) {
+          (this.newItem as ResponseDomain).responseKind = page.keys['ResponseKind'];
+        }
+
       }
     }
   }
@@ -65,4 +74,5 @@ export class TemplateComponent implements OnChanges, OnDestroy {
   ngOnDestroy(): void {
     this.alive = false;
   }
+
 }
