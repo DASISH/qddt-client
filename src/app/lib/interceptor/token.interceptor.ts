173358eb3b14d20921307170fe7e1ddc79c5e730
Stig Norland <stig.norland@nsd.no>
2021-02-20 13:54:26 +0100

    pre update

--- src/app/lib/interceptor/token.interceptor.ts
+++ src/app/lib/interceptor/token.interceptor.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpHeaders } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { TOKEN_NAME } from '../consts';
 
+import { TokenStorageService } from '../services/token-storage.service';
+
+const TOKEN_HEADER_KEY = 'Authorization';
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor() {
-    console.debug('TokenInterceptor');
-  }
+  constructor(private token: TokenStorageService) { }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem(TOKEN_NAME) || '';
-
-    // const authReq = request.clone({setHeaders: {Authorization: 'Bearer  ' + token}});
-    if (token) {
-      const newheader = new HttpHeaders().set('Authorization', 'Bearer ' + token)
-        .append('Access-Control-Allow-Origin', '*')
-        .append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      const authReq = request.clone({ headers: newheader });
-      return next.handle(authReq);
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // const token = localStorage.getItem(TOKEN_NAME)
+    let authReq = req
+    const token = this.token.getToken();
+    if (token != null) {
+      authReq = req.clone({
+        headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token)
+          .append('Access-Control-Allow-Origin', '*')
+          .append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+      })
     }
-    return next.handle(request);
+    return next.handle(authReq);
   }
 }
+
+export const authInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+]
+
+//   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+//     const token = localStorage.getItem(TOKEN_NAME) || '';
+
+//     // const authReq = request.clone({setHeaders: {Authorization: 'Bearer  ' + token}});
+//     if (token) {
+//       const newheader = new HttpHeaders().set('Authorization', 'Bearer ' + token)
+//         .append('Access-Control-Allow-Origin', '*')
+//         .append('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//       const authReq = request.clone({ headers: newheader });
+//       return next.handle(authReq);
+//     }
+//     return next.handle(request);
+//   }
+// }
